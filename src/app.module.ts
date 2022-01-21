2128974a357743ab4bd83f1aa67efb56059a4c8d
dowonkim97 <bluesky840712@gmail.com>
2022-01-21 22:32:34 +0900

    #5.7-2

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { UsersModule } from './users/users.module';
 import { CommonModule } from './common/common.module';
 import { User } from './users/entities/users.entity';
 import { JwtModule } from './jwt/jwt.module';
-import { JwtMiddleware } from './jwt/jwt.middleware';
+import { jwtMiddleware } from './jwt/jwt.middleware';
 
 // console.log(Joi);
 
@@ -58,10 +58,4 @@ import { JwtMiddleware } from './jwt/jwt.middleware';
   controllers: [],
   providers: [],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(JwtMiddleware)
-      .forRoutes({ path: '/graphql', method: RequestMethod.ALL });
-  }
-}
+export class AppModule {}
